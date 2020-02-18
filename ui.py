f30e98e64b765ece56cc7148bdb3affa0c2ca0a0
DM1122 <34867698+DM1122@users.noreply.github.com>
2020-02-17 20:35:46 -0500

    Separated UI into frames

--- ui.py
+++ ui.py
@@ -12,34 +12,69 @@ class GUI:
     def __init__(self, corpus):
         self.master = tkinter.Tk()
         self.corpus = corpus
-    
-        #region Widgets
-        self.master.title('Dictionary')
+
+        self.master.title('Dictionary Applet')
         self.master.iconbitmap('ui/book_icon.ico')
         
         self.intro_label = tkinter.Label(self.master, text='Welcome!')
-        
-        self.search_entry = tkinter.Entry(self.master)
-        self.search_button = tkinter.Button(self.master, text='Search', command=self.search)
+        self.intro_label.grid(row=0, column=0, sticky=tkinter.W)
 
-        self.dictionary_listbox = tkinter.Listbox(self.master)
-        
-        self.load_button = tkinter.Button(self.master, text='Import', command=self.load)
-        
-        self.log_label = tkinter.Label(self.master, text='Console:')
-        self.log = tkinter.Text(self.master)
+        #region Frames
+        self.browser_frame = tkinter.Frame(self.master)
+        self.ops_frame = tkinter.Frame(self.master)
+        self.lookup_frame = tkinter.Frame(self.master)
+        self.console_frame = tkinter.Frame(self.master)
 
-        self.delete_button = tkinter.Button(self.master, text='Delete', command=self.delete)
+        self.browser_frame.grid(row=1, column=0)
+        self.ops_frame.grid(row=1, column=1)
+        self.lookup_frame.grid(row=1, column=2)
+        self.console_frame.grid(row=2, column=0)
+
+        self.frames = (self.browser_frame, self.ops_frame, self.lookup_frame, self.console_frame)           # EXPERIMENTAL
         #endregion
+
+
+        #region Browser Frame
+        self.search_entry = tkinter.Entry(self.browser_frame)
+
+        self.search_button = tkinter.Button(self.browser_frame, text='Search', command=self.search)
+
+        self.dictionary_listbox = tkinter.Listbox(self.browser_frame, selectmode=tkinter.SINGLE)
+        #endregion
+
+        #region Ops Frame
+        self.load_button = tkinter.Button(self.ops_frame, text='Import', command=self.load)
         
+        self.delete_button = tkinter.Button(self.ops_frame, text='Delete', command=self.delete)
+
+        self.add_button = tkinter.Button(self.ops_frame, text='Add', command=self.add)
+        #endregion
+
+        #region Lookup Frame        
+        self.definition_label = tkinter.Label(self.lookup_frame, text='Definition:')
+        self.definition_text = tkinter.Text(self.lookup_frame)
+        #endregion
+
+        #region Console Frame
+        self.log_label = tkinter.Label(self.console_frame, text='Console:')
+        self.log = tkinter.Text(self.console_frame)
+        #endregion
+
+        for frame in self.frames:
+            for widget in frame.children:
+                frame.children[widget].pack()
+
         #region Layout
-        self.intro_label.grid(row=1, column=1, sticky=tkinter.W)
-        self.search_entry.grid(row=2, column=1, sticky=tkinter.W)
-        self.search_button.grid(row=2, column=2, sticky=tkinter.W)
-        self.dictionary_listbox.grid(row=3, column=1, sticky=tkinter.W)
-        self.load_button.grid(row=4, column=2)
-        self.log_label.grid(row=5, column=1, sticky=tkinter.W)
-        self.log.grid(row=6, column=1)
+        # self.search_entry.grid(row=2, column=1, sticky=tkinter.W)
+        # self.search_button.grid(row=2, column=2, sticky=tkinter.W)
+        # self.dictionary_listbox.grid(row=3, column=1, sticky=tkinter.W)
+        # self.load_button.grid(row=3, column=2)
+        # self.log_label.grid(row=5, column=1, sticky=tkinter.W)
+        # self.log.grid(row=6, column=1, columnspan=3, sticky=tkinter.W)
+        # self.add_button.grid(row=4, column=2)
+        # self.delete_button.grid(row=5, column=2)
+        # self.definition_label.grid(row=2, column=3)
+        # self.definition_text.grid(row=3, column=3, rowspan=1)
         #endregion
 
     
@@ -52,8 +87,9 @@ class GUI:
 
 
     def search(self):
-        query = self.search_bar.get()
-        self.printlog('Searching for '+query)
+        query = self.search_entry.get()
+        self.printlog('Searching for {} in dictionary'.format(query))
+        print(self.corpus.search(query))
 
     
     def printlog(self, text):
@@ -68,7 +104,16 @@ class GUI:
             self.dictionary_listbox.insert(tkinter.END, node.data[0])
 
     def delete(self):
-        pass
+        idx = self.dictionary_listbox.curselection()
+        key = self.dictionary_listbox.get(idx)
+        self.printlog('Deleting "{}" from dictionary'.format(key))
+
+        # self.corpus.delete(key)
+
+    def add(self):
+        self.printlog('Adding "{}" from dictionary'.format('x'))
+
+        # self.corpus.add(key)
 
 
 
